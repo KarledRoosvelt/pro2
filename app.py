33c9ce90157112dd25349f1d61455126901ea130
kenfack <roosveltkenfi@gmail.com>
2026-01-02 23:33:13 +0100

    modif

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ st.markdown("""
     .metric-container {background-color: #f8f9fa; padding: 1rem; border-radius: 8px;}
  
     :root {
-    --bg-color: #5E3D37;      /* bleu ciel clair */
+    --bg-color: #EAF4FF;      /* bleu ciel clair */
     --text-dark: #1E3A5F;    /* bleu foncé lisible */
 }
 
