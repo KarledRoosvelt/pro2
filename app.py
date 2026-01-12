d3da9ad6d856ab941dd09624f30d7371a9e61886
kenfack <roosveltkenfi@gmail.com>
2026-01-12 09:03:40 +0100

    modifications

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ st.markdown("""
     .metric-container {background-color: #f8f9fa; padding: 1rem; border-radius: 8px;}
  
     :root {
-    --bg-color: #C3C8DB;      /* bleu ciel clair */
+    --bg-color: #D1D6E6;      /* bleu ciel clair */
     --text-dark: #1E3A5F;    /* bleu foncé lisible */
 }
 
